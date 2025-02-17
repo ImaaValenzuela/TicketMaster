ab5e4fdfcdad7a92375c0aaf2d66815c40736ff6
ImaaValenzuela <imanolvalenzuela03@gmail.com>
2025-02-17 00:41:08 -0300

    Implement Conditional Rendering and List Rendering

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return(
+        <div>
+            <p>Eventos Disponibles</p>
+            <input type="text" placeholder="Busca tu evento favorito"/>
+        </div>
+    )
+}
+
+export default Navbar
